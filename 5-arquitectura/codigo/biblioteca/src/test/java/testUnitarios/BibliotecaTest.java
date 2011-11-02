f3c1cd242b94c4dd825239192b0642070ccf43d8
cdmarchionne@gmail.com <cdmarchionne@gmail.com@a8a4d2d6-1de0-d657-905b-3aa8a874b0b4>
2011-11-02 06:37:51 +0000

    Ant Funcionando

--- 5-arquitectura/codigo/biblioteca/src/test/java/testUnitarios/BibliotecaTest.java
+++ 5-arquitectura/codigo/biblioteca/src/test/java/testUnitarios/BibliotecaTest.java
@@ -5,18 +5,12 @@ import junit.framework.TestCase;
 import modelo.Biblioteca;
 import modelo.Libro;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-//import org.mockito.Mockito.*;
-
 import persistencia.Repositorio;
 import persistencia.RepositorioUnitario;
 
 /**
  * @author cristian
- *
+ * 
  */
 @SuppressWarnings("unchecked")
 public class BibliotecaTest extends TestCase {
@@ -26,21 +20,20 @@ public class BibliotecaTest extends TestCase {
 	/**
 	 * Seteo el Repositorio
 	 */
-	@Before
 	public void setUp() throws Exception {
 		RepositorioUnitario<Biblioteca> repositorioBiblioteca = new RepositorioUnitario<Biblioteca>();
 		repositorioBiblioteca.insertar(new Biblioteca());
-		Repositorio.getInstancia().agregar(Biblioteca.BIBLIOTECA,repositorioBiblioteca);
+		Repositorio.getInstancia().agregar(Biblioteca.BIBLIOTECA, repositorioBiblioteca);
 	}
-	
+
 	/**
 	 * Vacio la Biblioteca
 	 */
-	@After
 	protected void tearDown() throws Exception {
-		RepositorioUnitario<Biblioteca> repoBiblio = (RepositorioUnitario<Biblioteca>) Repositorio.getInstancia().obtener(Biblioteca.BIBLIOTECA);
+		RepositorioUnitario<Biblioteca> repoBiblio = (RepositorioUnitario<Biblioteca>) Repositorio
+				.getInstancia().obtener(Biblioteca.BIBLIOTECA);
 		Biblioteca biblioteca = repoBiblio.obtenerElemento();
-		
+
 		for (Libro libro : biblioteca.obtenerTodosLosLibros()) {
 			biblioteca.sacarLibro(libro);
 		}
@@ -51,16 +44,16 @@ public class BibliotecaTest extends TestCase {
 	 * Test de ESCRITURA
 	 * Test method for {@link modelo.Biblioteca#addLibro(modelo.Libro)}.
 	 */
-	@Test
 	public void testAddLibro() {
 		final String ISBN = "1234";
 
-		RepositorioUnitario<Biblioteca> repoBiblio = (RepositorioUnitario<Biblioteca>) Repositorio.getInstancia().obtener(Biblioteca.BIBLIOTECA);
+		RepositorioUnitario<Biblioteca> repoBiblio = (RepositorioUnitario<Biblioteca>) Repositorio
+				.getInstancia().obtener(Biblioteca.BIBLIOTECA);
 		Biblioteca biblioteca = repoBiblio.obtenerElemento();
-		
+
 		Libro libro = new Libro(ISBN, "El naufrago", "Desconocido", "Nautilus");
 		biblioteca.agregarLibro(libro);
-		
+
 		assertEquals(MENSAJE_TEST_ADD_LIBRO, libro, biblioteca.obtenerLibroPorIsbn(ISBN));
 	}
 
@@ -68,16 +61,16 @@ public class BibliotecaTest extends TestCase {
 	 * Test de LECTURA
 	 * Test method for {@link modelo.Biblioteca#buscarIsbn(java.lang.String)}.
 	 */
-	@Test
 	public void testBuscarIsbn() {
 		final String ISBN = "1234";
-		
-		RepositorioUnitario<Biblioteca> repoBiblio = (RepositorioUnitario<Biblioteca>) Repositorio.getInstancia().obtener(Biblioteca.BIBLIOTECA);
+
+		RepositorioUnitario<Biblioteca> repoBiblio = (RepositorioUnitario<Biblioteca>) Repositorio
+				.getInstancia().obtener(Biblioteca.BIBLIOTECA);
 		Biblioteca biblioteca = repoBiblio.obtenerElemento();
-		
+
 		Libro libro = new Libro(ISBN, "El naufrago", "Desconocido", "Nautilus");
 		biblioteca.agregarLibro(libro);
-		
+
 		assertFalse(MENSAJE_TEST_ADD_LIBRO, biblioteca.buscarIsbn(ISBN).isEmpty());
 	}
 
