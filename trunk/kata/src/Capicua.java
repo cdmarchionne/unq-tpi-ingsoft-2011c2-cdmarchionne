4c66cfae71c996ebde66a847e5781acde9c9f447
cdmarchionne@gmail.com <cdmarchionne@gmail.com@a8a4d2d6-1de0-d657-905b-3aa8a874b0b4>
2011-11-14 23:39:22 +0000

    Se agregaron más casos de Test y se modificó el comportamiento de la comprobación de Capicúa

--- trunk/kata/src/Capicua.java
+++ trunk/kata/src/Capicua.java
@@ -1,8 +1,22 @@
 
 public class Capicua {
 
-	public static boolean comprobar(String string) {
-		return (string=="ANANA");
+	public static boolean comprobar(String palabra) {
+		int top = 0;
+		int bottom = palabra.length()-1;
+		final char[] palabraToArray = palabra.toCharArray();
+		
+		if (!(palabra.isEmpty())) {
+			while(bottom>top){
+				if (!(palabraToArray[top]==palabraToArray[bottom])) {
+					return false;
+				}
+				top++;
+				bottom--;
+			}
+		}
+			
+		return true;
 	}
 
 }
--- trunk/kata/test/CapicuaTest.java
+++ trunk/kata/test/CapicuaTest.java
@@ -3,12 +3,24 @@ import junit.framework.TestCase;
 
 public class CapicuaTest extends TestCase {
 	
-	public void testShouldAssetIfExampleAnana(){
+	public void testShouldAssertIfExampleAnana(){
 		assertTrue(Capicua.comprobar("ANANA"));
 	}
 	
 	public void testShouldFailIfExampleBanana(){
 		assertFalse(Capicua.comprobar("BANANA"));
+	}
+	
+	public void testShouldAssertIfExampleEmpty(){
+		assertTrue(Capicua.comprobar(""));
+	}
+	
+	public void testShouldAssertIfExampleEven(){
+		assertTrue(Capicua.comprobar("22"));
+	}
+	
+	public void testShouldAssertIfExampleOdd(){
+		assertTrue(Capicua.comprobar("111"));
 	}
 	
 }
