7bb1d5244692be2d71ccb591117a3bc036fd2f3a
cdmarchionne@gmail.com <cdmarchionne@gmail.com@a8a4d2d6-1de0-d657-905b-3aa8a874b0b4>
2011-11-15 00:13:17 +0000

    Se agrego el caso de prueba para calcular el Hash de palabras distintas

--- kata/src/Hash.java
+++ kata/src/Hash.java
@@ -3,9 +3,17 @@ public class Hash {
 
 	public static Integer obtenerHash(String palabra) {
 		if(palabra!=null){
-			return 0;
+			return hash(palabra);
 		}
 		return null;
+	}
+	
+	public static Integer hash(String palabra) {
+		Integer acumulador=0;
+		for(int i=0 ; i<palabra.length() ; i++){
+			acumulador = acumulador + palabra.charAt(i);
+		}
+		return acumulador;
 	}
 
 }
